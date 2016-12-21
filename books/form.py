9d2ad191480d4cc38ec1a4eb64259fdae5eacfa7
houxr <houxr@ngarihealth.com>
2016-12-21 11:23:55 +0800

    form modify

--- books/form.py
+++ books/form.py
@@ -1,6 +1,11 @@
+#! /usr/bin/python
+# -*- coding:utf-8 -*-
+
 from django.forms import forms
 
-class LenderForm(forms.Form):
+
+# view 对应的所有 Form 表单
+class BorrowerForm(forms.Form):
     bookNo = forms.CharField()
     title = forms.CharField()
     email = forms.EmailField(required=False)
